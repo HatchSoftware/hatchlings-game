6bce803bc45c5305b03a665e350ca62270eda5ee
Peter Leconte <peter.leconte@hatch.be>
2024-03-15 11:19:25 +0100

    added init lobby

---
+++ scenes/ui/lobby.gd
@@ -0,0 +1,27 @@
+extends Control
+
+@onready var player_list: ItemList = %PlayerList
+@onready var start_game_btn: Button = %StartGame
+
+@onready var world_scene := preload("res://scenes/world.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Lobby.player_connected.connect(func(_id, _info): _refresh_player_list())
+	Lobby.player_disconnected.connect(func(_id, _info): _refresh_player_list())
+	_refresh_player_list()
+	if !multiplayer.is_server():
+		start_game_btn.hide()
+
+func _refresh_player_list():
+	player_list.clear()
+	for p in Lobby.players:
+		var info = Lobby.players[p]
+		var text = info.name
+		if p == multiplayer.get_unique_id():
+			text += " (You!)"
+		player_list.add_item(text)
+
+func _on_start_game_pressed():
+	Lobby.load_game.rpc("res://scenes/world.tscn")
+
--- scenes/ui/menus/main_menu.gd
+++ scenes/ui/menus/main_menu.gd
@@ -1,7 +1,48 @@
 extends Control
 
-var world = preload("res://scenes/world.tscn")
+
+@onready var player_name_text: TextEdit = %PlayerName
+@onready var ip_text: TextEdit = %IP
+@onready var port_text: TextEdit = %Port
+
+@onready var player_list: ItemList = %PlayerList
+
+var lobby_scene = preload("res://scenes/ui/lobby.tscn")
 
 
-func _on_button_pressed():
-	get_tree().change_scene_to_packed(world)
+func _ready():
+	# Lobby.player_connected.connect(func(_id, _info): _refresh_player_list())
+	# Lobby.player_disconnected.connect(func(_id, _info): _refresh_player_list())
+	# start_lobby_screen()
+	pass
+
+func start_lobby_screen():
+	get_tree().change_scene_to_packed(lobby_scene)
+
+func _refresh_player_list():
+	player_list.clear()
+	for p in Lobby.players:
+		var info = Lobby.players[p]
+		var text = info.name
+		if p == multiplayer.get_unique_id():
+			text += " (You!)"
+		player_list.add_item(text)
+	pass
+
+
+func _on_join_btn_pressed():
+	Lobby.player_info = {
+		"name": player_name_text.text
+	}
+	Lobby.join_game(ip_text.text, int(port_text.text))
+	start_lobby_screen()
+
+
+
+func _on_host_btn_pressed():
+	Lobby.player_info = {
+		"name": player_name_text.text
+	}
+	Lobby.create_game(int(port_text.text))
+	start_lobby_screen()
+
